d7029babd621e89142a75240f3b253ac863995b0
Vladislav <vambrosevich@gmail.com>
2017-09-05 18:06:17 +0300

    Add interface, implementation
    Add IDynamicList<T> interface
    Add DynamicList<T> implementation
    Add console prj for tests

---
+++ Lab_1/Lab1.Collections.Generic/IDynamicList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Collections.Generic
+{
+    public interface IDynamicList<T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        int Capacity { get; set; }
+
+        T this[int index] { get; set; }
+
+
+        void Add(T item);
+
+        bool Remove(T item);
+
+        void RemoveAt(int index);
+
+        void Clear();
+    }
+}
---
+++ Lab_1/Lab1.MainProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Lab1.Collections.Generic;
+
+namespace Lab1.MainProject
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var list = new DynamicList<int>{1, 2, 3, 4, 5};
+            Console.WriteLine($"Initial length: { list.Count }");
+            Console.WriteLine("Initial items:");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.Add(6);
+            list.Add(8);
+            Console.WriteLine("Items after adding:");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            Console.WriteLine("Items after removing by index:");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.Remove(6);
+            list.Remove(8);
+            Console.WriteLine("Items after removing by value:");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.Clear();
+            Console.WriteLine($"Length after clearing: { list.Count }");
+            Console.ReadKey();
+        }
+    }
+}
