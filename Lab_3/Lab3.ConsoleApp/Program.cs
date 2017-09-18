3e484e3c31cc0bd0ca28f70aa053b3a58d378d92
Vladislav <vambrosevich@gmail.com>
2017-09-19 01:34:51 +0300

    Add sync files copying

---
+++ Lab_3/Lab3.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lab3.Copying;
+
+namespace Lab3.ConsoleApp
+{
+    internal class Program
+    {
+        private const string SrcPath = "./Src";
+        private const string DestPath = "./Dest2";
+
+
+        private static void Main(string[] args)
+        {
+            //ThreadPool.QueueUserWorkItem()
+            var filesCount = ParallelFileCopier.DeepCopying(SrcPath, DestPath, true);
+            Console.WriteLine(filesCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_3/Lab3.Copying/ParallelFileCopier.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
+using System.IO;
+using System.Threading;
+
+namespace Lab3.Copying
+{
+    public static class ParallelFileCopier
+    {
+        public static int DeepCopying(string srcPath, string destPath, bool overRide)
+        {
+            if (!Directory.Exists(destPath))
+            {
+                CreateDirectory(destPath);
+            }
+            else
+            {
+                if (overRide)
+                {
+                    Directory.Delete(destPath, true);
+                }
+                else
+                {
+                    throw new DirectoryException(nameof(destPath));
+                }
+            }
+
+            //var doneEvents = new List<ManualResetEvent>();
+
+            foreach (var directory in Directory.GetDirectories(srcPath, "*.*", System.IO.SearchOption.AllDirectories))
+            {
+                ThreadPool.QueueUserWorkItem(CreateDirectory, directory.Replace(srcPath, destPath));
+                //CreateDirectory(directory.Replace(srcPath, destPath));
+            }
+
+            var copiedFilesCount = 0;
+
+            foreach (var file in Directory.GetFiles(srcPath, "*.*", System.IO.SearchOption.AllDirectories))
+            {
+
+                ThreadPool.QueueUserWorkItem(CopyFile, new FileCopyingParameters{ SrcPath = file, DestPath = file.Replace(srcPath, destPath) });
+                copiedFilesCount++;
+            }
+            //WaitHandle.WaitAll();
+
+            return copiedFilesCount;
+        }
+
+
+        private static void CopyFile(Object pathParams)
+        {
+            var copiedParams = (FileCopyingParameters) pathParams;
+            var srcPath = copiedParams.SrcPath;
+            var destPath = copiedParams.DestPath;
+            File.Copy(srcPath, destPath, true);
+        }
+
+
+
+        private static void CreateDirectory(object path)
+        {
+            Directory.CreateDirectory((string)path);
+        }
+
+
+
+        private class FileCopyingParameters
+        {
+            public string SrcPath { get; set; }
+            public string DestPath { get; set; }
+        }
+    }
+}
