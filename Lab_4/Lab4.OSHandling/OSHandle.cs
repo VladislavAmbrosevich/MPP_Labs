3a91a4b14a5f493f04842ec9d5d5fd505491e39a
Vladislav <vambrosevich@gmail.com>
2017-09-25 14:20:10 +0300

    Add OsHandle

---
+++ Lab_4/Lab4.OSHandling/OSHandle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lab4.OSHandling
+{
+    public class OsHandle : IDisposable
+    {
+        private bool _disposed;
+
+
+        public IntPtr Handle { get; set; }
+
+        ~OsHandle()
+        {
+            Dispose(false);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Release managed resources.
+                }
+                // Release unmanaged resources.
+                if (Handle != IntPtr.Zero)
+                {
+                    Marshal.FreeCoTaskMem(Handle);
+                    Handle = IntPtr.Zero;
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Lab_4/Lab4.OSHandling/SafeOSHandle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.OSHandling
+{
+    public class SafeOsHandle : SafeHandle
+    {
+        public SafeOsHandle(IntPtr invalidHandleValue, bool ownsHandle) : base(invalidHandleValue, ownsHandle)
+        {
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsInvalid { get; }
+    }
+}
