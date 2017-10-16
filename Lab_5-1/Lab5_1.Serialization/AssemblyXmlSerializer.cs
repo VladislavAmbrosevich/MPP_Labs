1502cc75748843197cdb24d1b667e05de5e3aa46
Vladislav <vambrosevich@gmail.com>
2017-10-16 10:25:25 +0300

    Add xdocument (end, dirty)

--- Lab_5-1/Lab5_1.Serialization/AssemblyXmlSerializer.cs
+++ Lab_5-1/Lab5_1.Serialization/AssemblyXmlSerializer.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
+using Lab5_1.Common;
 using Lab5_1.Serialization.TypeMembersDescriptions;
 
 namespace Lab5_1.Serialization
@@ -20,12 +21,46 @@ namespace Lab5_1.Serialization
                     from TypeInfo typeInfo in typesInfo
                     orderby typeInfo.Name descending
                     select new XElement(XmlNames.ClassTag,
-                        from FieldDescription field in typeInfo.Fields
-                        select new XElement(XmlNames.FieldTag,
-                            new XAttribute(XmlNames.NameAttribute, field.Name),
-                            new XAttribute(XmlNames.AccessModifierAttribute, field.AccessModifier.GetString()),
-                            new XAttribute(XmlNames.TypeAttribute, field.Type.Name)),
-                        new XAttribute(XmlNames.NameAttribute, typeInfo.Name)
+                        new XAttribute(XmlNames.NameAttribute, typeInfo.Name),
+
+                        typeInfo.Fields.Count != 0
+                            ? new XElement(XmlNames.FieldsTag, 
+                                from FieldDescription field in typeInfo.Fields
+                                select new XElement(XmlNames.FieldTag,
+                                    new XAttribute(XmlNames.NameAttribute, field.Name),
+                                    new XAttribute(XmlNames.AccessModifierAttribute, field.AccessModifier.GetString()),
+                                    new XAttribute(XmlNames.TypeAttribute, field.Type.Name)))
+                            : null,
+
+                        typeInfo.Methods.Count != 0
+                            ? new XElement(XmlNames.MethodsTag,
+                                from MethodDescription method in typeInfo.Methods
+                                select new XElement(XmlNames.MethodTag,
+                                    new XAttribute(XmlNames.NameAttribute, method.Name),
+                                    new XAttribute(XmlNames.AccessModifierAttribute, method.AccessModifier.GetString()),
+                                    new XAttribute(XmlNames.ReturnTypeAttribute, method.ReturnType.Name),
+                                    method.Params.Count != 0
+                                        ? new XElement(XmlNames.ParametersTag,
+                                            from ParameterInfo parameter in method.Params
+                                            select new XElement(XmlNames.ParameterTag,
+                                                new XAttribute(XmlNames.NameAttribute, parameter.Name),
+                                                new XAttribute(XmlNames.TypeAttribute, parameter.Type.Name)))
+                                        : null ))
+                            : null,
+
+                        typeInfo.ImplementedInterfaces.Count != 0
+                            ? new XElement(XmlNames.ImplementedInterfacesTag,
+                                from Type implementedInterface in typeInfo.ImplementedInterfaces
+                                select new XElement(XmlNames.InterfaceTag,
+                                    new XAttribute(XmlNames.NameAttribute, implementedInterface.Name))) 
+                            : null,
+
+                        typeInfo.InheritedTypes.Count != 0
+                            ? new XElement(XmlNames.InheritedClassesTag,
+                                from Type inheritedClass in typeInfo.InheritedTypes
+                                select new XElement(XmlNames.InheritedClassTag,
+                                    new XAttribute(XmlNames.NameAttribute, inheritedClass.Name)))
+                            : null
 
                     )
                 ));
@@ -42,7 +77,8 @@ namespace Lab5_1.Serialization
         private static List<TypeInfo> GetAssemblyTypesInfo(Assembly assembly)
         {
             var typesInfo = new List<TypeInfo>();
-            foreach (var type in assembly.GetTypes())
+            var types = assembly.GetTypes().Where(type => type.IsDefined(typeof(ExportXmlAttribute), false)).ToList();
+            foreach (var type in types)
             {
                 var typeInfo = TypeParser.GetTypeInfo(type);
                 typesInfo.Add(typeInfo);
--- Lab_5-1/Lab5_1.Serialization/XmlNames.cs
+++ Lab_5-1/Lab5_1.Serialization/XmlNames.cs
@@ -6,16 +6,21 @@
         public const string FieldsTag = "Fields";
         public const string MethodsTag = "Methods";
         public const string ParametersTag = "Parameters";
+        public const string ImplementedInterfacesTag = "ImplementedInterfaces";
+        public const string InheritedClassesTag = "InheritedClasses";
 
         public const string ClassTag = "Class";
         public const string FieldTag = "Field";
         public const string MethodTag = "Method";
         public const string ParameterTag = "Parameter";
         public const string AssemblyTag = "Assembly";
+        public const string InterfaceTag = "Interface";
+        public const string InheritedClassTag = "InheritedClass";
 
         public const string NameAttribute = "Name";
         public const string TypeAttribute = "Type";
         public const string AccessModifierAttribute = "AccessModifier";
+        public const string ReturnTypeAttribute = "ReturnType";
 
     }
 }
